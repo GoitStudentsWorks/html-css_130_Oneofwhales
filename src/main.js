07b00715e58a5bb2050222021aedc151052adf4b
Artem Ishchenko <nordeptanah8@gmail.com>
2026-06-14 14:29:31 +0300

    Add toggle backgound to back to top btn

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ questionBtn.forEach(btn => {
 
 //JS for back to top btn
 const backToTopBtn = document.querySelector('.back-to-top-btn');
+const section = document.querySelector('.benefits');
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > 300) {
@@ -18,6 +19,16 @@ window.addEventListener('scroll', () => {
   } else {
     backToTopBtn.classList.remove('is-visible');
   }
+
+  const sectionRect = section.getBoundingClientRect();
+
+  const buttonTop = window.innerHeight - 30 - backToTopBtn.offsetHeight;
+  const buttonBottom = buttonTop + backToTopBtn.offsetHeight;
+
+  const isOverSection =
+    buttonBottom > sectionRect.top && buttonTop < sectionRect.bottom;
+
+  backToTopBtn.classList.toggle('is-benefits', isOverSection);
 });
 
 backToTopBtn.addEventListener('click', () => {
