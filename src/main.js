c0a8550c2c300eee56ada1bf9677386bce26f2a8
Artem Ishchenko <nordeptanah8@gmail.com>
2026-06-14 21:39:35 +0300

    Refactoring JS code, add events for menu btns

--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
+//Utils
+function getElementByClass(elClass) {
+  return document.querySelector(elClass);
+}
+
+function toggleClass(el, elClass, condition) {
+  return condition === undefined
+    ? el.classList.toggle(elClass)
+    : el.classList.toggle(elClass, condition);
+}
+
+function toggleMenu(mobileMenu, headerContainer, condition) {
+  toggleClass(mobileMenu, 'is-open', condition);
+  toggleClass(headerContainer, 'is-open-mobile', condition);
+}
+
 // JS for button faq section
 const questionBtn = document.querySelectorAll('.question-btn');
 
@@ -5,20 +21,17 @@ questionBtn.forEach(btn => {
   btn.addEventListener('click', () => {
     const item = btn.closest('.question-list-item');
 
-    item.classList.toggle('is-open');
+    toggleClass(item, 'is-open');
   });
 });
 
 //JS for back to top btn
 const backToTopBtn = document.querySelector('.back-to-top-btn');
-const section = document.querySelector('.benefits');
+const section = getElementByClass('.benefits');
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 300) {
-    backToTopBtn.classList.add('is-visible');
-  } else {
-    backToTopBtn.classList.remove('is-visible');
-  }
+  const isScroll = window.scrollY > 300;
+  toggleClass(backToTopBtn, 'is-visible', isScroll);
 
   const sectionRect = section.getBoundingClientRect();
 
@@ -28,11 +41,37 @@ window.addEventListener('scroll', () => {
   const isOverSection =
     buttonBottom > sectionRect.top && buttonTop < sectionRect.bottom;
 
-  backToTopBtn.classList.toggle('is-benefits', isOverSection);
+  toggleClass(backToTopBtn, 'is-benefits', isOverSection);
 });
 
 backToTopBtn.addEventListener('click', () => {
   window.scrollTo({
     top: 0,
   });
+});
+
+//header menu
+const burgerMenuBtn = getElementByClass('.burger-btn');
+const closeMenuBtn = getElementByClass('.close-btn');
+const mobileMenu = getElementByClass('.mobile-tablet-menu-container');
+const headerContainer = getElementByClass('.header-container');
+
+burgerMenuBtn.addEventListener('click', () => {
+  toggleMenu(mobileMenu, headerContainer, true);
+});
+
+closeMenuBtn.addEventListener('click', () => {
+  toggleMenu(mobileMenu, headerContainer, false);
+});
+
+document.addEventListener('keydown', event => {
+  if (event.key === 'Escape') {
+    toggleMenu(mobileMenu, headerContainer, false);
+  }
+});
+
+mobileMenu.addEventListener('click', event => {
+  if (event.target === mobileMenu) {
+    toggleMenu(mobileMenu, headerContainer, false);
+  }
 });
